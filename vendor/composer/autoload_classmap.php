98ea9ab20cba863192da1c9786210e6a642b6805
REYESc137 <juliantuyub05@gmail.com>
2024-11-05 19:40:47 -0600

    actualizaciones de sistema

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -24,6 +24,7 @@ return array(
     'App\\Http\\Controllers\\Auth\\VerifyEmailController' => $baseDir . '/app/Http/Controllers/Auth/VerifyEmailController.php',
     'App\\Http\\Controllers\\Controller' => $baseDir . '/app/Http/Controllers/Controller.php',
     'App\\Http\\Controllers\\DescubridoresController' => $baseDir . '/app/Http/Controllers/DescubridoresController.php',
+    'App\\Http\\Controllers\\FaceApiController' => $baseDir . '/app/Http/Controllers/FaceApiController.php',
     'App\\Http\\Controllers\\GoogleController' => $baseDir . '/app/Http/Controllers/GoogleController.php',
     'App\\Http\\Controllers\\PageController' => $baseDir . '/app/Http/Controllers/PageController.php',
     'App\\Http\\Controllers\\ProfileController' => $baseDir . '/app/Http/Controllers/ProfileController.php',
